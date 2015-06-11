3291a81863d6fe4ca8584ff24c7c08e4f0da61d6
Gaston Sanchez <gaston.stat@gmail.com>
2015-06-10 20:59:21 -0700

    add lab02

---
+++ HW1/homework01.R
@@ -0,0 +1,155 @@
+# =====================================================
+# Stat133: HW 1
+# Description: Basics of character vectors
+# Data: Teams that played the superbowl
+# =====================================================
+
+# Write your name
+# Name:
+
+
+# Start a new R session and load the data
+load(url("http://gastonsanchez.com/teaching/stat133/superbowl_teams.RData"))
+
+# "superbowl_teams.RData" contains 3 vectors:
+# year: year of superbowl from 1967 to 2015
+# winner: champions
+# loser: losing teams
+
+# find the class of each vector (year, winner, loser)
+
+
+
+# use length() to determine whether the three vectors
+# have the same number of elements
+
+
+
+
+# =====================================================
+# Winning teams
+# Write the commands to answer the following questions
+# =====================================================
+
+# get the first 5 champions
+
+
+
+# get the last 5 champions
+
+
+# how many unique champions?
+
+
+# use the function table() to get a 
+# table of frequencies for the winning teams
+# (assign the table to the object 'win_freqs')
+
+
+# what team has won the superbowl most times?
+# and how many times?
+
+
+
+# apply table() on 'win_freqs', this will give you
+# how many teams have won how many superbowls
+
+
+
+# create a vector 'champions' by sorting the frequencies
+# 'win_freqs' in decreasing order
+
+
+# make a barplot of 'champions' with barplot()
+
+
+# try this command
+barplot(champions, las = 2)
+
+# try this other command
+op <- par(mar = c(2, 11, 1, 2))
+barplot(champions, horiz = TRUE, las = 2)
+par(op)
+
+
+# What are the championships of "San Francisco 49ers"
+
+
+# What are the championships of "Oakland Raiders" 
+
+
+# when was the last time Denver Broncos won a superbowl?
+
+
+
+
+# create 'winner2', a copy of the vector 'winner'
+
+
+# replace some team names in 'winner2' as follows:
+# "New York Giants" to "NYG"
+# "New York Jets" to "NYJ"
+# "Kansas City Chiefs" to "KCC"
+
+
+
+
+# =====================================================
+# Losing team
+# Write the commands to answer the following questions
+# =====================================================
+
+# get the losing teams of the first 5 superbowls
+
+
+
+# get the losing teams of the last 5 superbowls
+
+
+# create the frequency table 'los_freqs'
+# of losing teams
+
+
+# What is the team that have lost the superbowl
+# the most times?, and how many times?
+
+
+
+
+# =====================================================
+# Winners and Losers
+# Write the commands to answer the following questions
+# =====================================================
+
+# how many different teams have played the superbowl?
+
+
+# teams that have played the superbowl and have never lost
+
+
+# teams that have played the superbowl and have never won
+
+
+# teams that have played the superbowl (both won and lost)?
+
+
+# how many teams have both won and lost the superbowl?
+
+
+
+# what team won the superbowl in 2000
+
+
+# what team lost the superbowl in 2000
+
+
+# what teams won the superbowl in the 1970s (1970-1979)
+
+
+# what teams lost the superbowl in the 1990s (1990-1999)
+
+
+# create a data frame 'superbowl' with the three vectors:
+# year, winner, loser
+
+
---
+++ lab2/lab02.R
@@ -0,0 +1,189 @@
+# =====================================================
+# Stat133: Lab 2
+# Description: Basics of data frames
+# Data: Star Wars characters
+# =====================================================
+
+# To read the data, we'll use the package "readr"
+# To install "readr" type:
+install.packages("readr")
+
+# Remember that you only need to install a package once
+
+# load "readr
+library("readr")
+
+# read data using read_csv()
+# (read_csv() does not convert strings into factors)
+sw <- read_csv("https://raw.githubusercontent.com/gastonstat/stat133/master/datasets/starwarstoy.csv")
+
+
+# use str() to get information about the data frame structure
+
+
+# use summary() to get some descriptive statistics
+
+
+# convert column 'gender' as a factor
+
+
+# apply summary() on gender
+
+
+# apply table() on gender
+
+
+# what's the largest height value
+
+
+# what's the minimum height value
+
+
+# what's the average height
+
+
+# who has of smallest height
+
+
+# who has of smallest height
+
+
+# sort height
+
+
+# sort weight in decrasing order
+
+
+# are there any subjects with weapon knife
+
+
+# are there any subjects with height > 2 meters
+
+
+# are there any subjects with height < 0.5 meters
+# or weight < 50 kg
+
+
+# what are the unique weapons
+
+
+# how many different species
+
+
+
+# =====================================================
+# Subsetting operations (without using subset())
+# =====================================================
+
+# first row
+
+
+# Han Solo's info
+
+
+# subject's info of smallest height
+
+
+# male subjects
+
+
+# less than 1.7 m height
+
+
+# less than 1.7 m height, and less than 50 g weight
+
+
+# select data of droids or humans
+
+
+# select data of non-humans
+
+
+
+# =====================================================
+# Subsetting operations using subset()
+# =====================================================
+
+# Han Solo's info
+
+
+# subject's info of smallest height
+
+
+# male subjects
+
+
+# less than 1.7 m height
+
+
+# less than 1.7 m height, and less than 50 g weight
+
+
+# select data of droids or humans
+
+
+# select data of non-humans
+
+
+
+# =====================================================
+# Sorting operations
+# =====================================================
+
+# sort data by height
+
+
+# sort data by weight in decreasing order
+
+
+# sort data by gender
+
+
+# sort data by species
+
+
+
+# =====================================================
+# Additional questions
+# =====================================================
+
+# information of jedis
+
+
+# names of non-jedis
+
+
+# data of non humans
+
+
+# who are above average weight?
+
+
+# how many unarmed humans?
+
+
+# standard deviation of weight in non-humans 
+
+
+# which humans use a lightsaber
+
+
+# median height of unarmed non-humans
+
+
+# get the square weight of non-jedis
+
+
+# get the log of height for subjects
+# with blaster or bowcaster weapons
+
+
+
+# make a plot with height and weight
+
+
+# create a new variable "newvar": height divided by weight
+
+
+# add 'newvar' to the data frame sw
+
